5494cd831b14127138b55d27d6d6e319442fd85e
tanisha-joshi <tanishajoshi121@gmail.com>
2024-01-26 00:37:56 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Home from './Home';
 import CreateWallet from './CreateWallet';
 import WalletHome from './WalletHome';
 import Finish from './Finish';
+import SendHome from './SendHome'
 
 function App() {
   return (
     <div className="App h-500 bg-white">
-      <WalletHome />
+      {/* <WalletHome /> */}
       {/* <Home /> */}
       {/* <CreateWallet seedphrase={"palm shiver eager merge solve hard master foot produce bulb zebra hockey"} /> */}
       {/* <Finish address= {"0x5A0dFfe964188E62C1acc3C6a032D9cC57B1CfC9"} /> */}
+      <SendHome />
     </div>
   );
 }
